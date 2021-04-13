a62eae36cdf031c388197f096c8b06760ca48d58
Martin Törnqvist <gris_martin@hotmail.com>
2021-04-13 17:58:44 +0200

    Add functions to convert between world and screen

--- Mapper/MainViewModel.cs
+++ Mapper/MainViewModel.cs
@@ -12,7 +12,34 @@ namespace Mapper
 
         public ObservableCollection<MapSymbolModel> MapSymbols { get; } = new ObservableCollection<MapSymbolModel>();
 
-        public Point Center { get; set; }
+        /// <summary>
+        /// Current world coordinate of lower left corner
+        /// </summary>
+        public Point Origin { get; set; }
+
+        /// <summary>
+        /// Scale in meters per pixel
+        /// </summary>
         public double Scale { get; set; }
+
+
+        public Point ToWorldCoordinate(Point screenPoint, double screenHeight)
+        {
+            var screenX = screenPoint.X;
+            var screenY = screenPoint.Y;
+            var worldX = screenX * Scale + Origin.X;
+            var worldY = (screenHeight - screenY) * Scale + Origin.Y;
+            return new Point(worldX, worldY);
+        }
+
+
+        public Point ToScreenCoordinate(Point worldPoint, double screenHeight)
+        {
+            var worldX = worldPoint.X;
+            var worldY = worldPoint.Y;
+            var screenX = (worldX - Origin.X) / Scale;
+            var screenY = (worldY - Origin.Y) / Scale - screenHeight;
+            return new Point(screenX, screenY);
+        }
     }
 }
