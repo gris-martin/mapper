114aada3f3702afc1bbe268ab4a1d766955b0ce5
Martin Törnqvist <gris_martin@hotmail.com>
2021-04-19 20:28:43 +0200

    Use map view model to give popup properties

--- Mapper/Controls/MapView.xaml.cs
+++ Mapper/Controls/MapView.xaml.cs
@@ -15,8 +15,8 @@ namespace Mapper.Controls
     {
         private Point lastRightClickPosition = new();
         private bool isPanning = false;
-        private bool isMeasuring = false;
         private Point lastPanPosition = new();
+        private MapViewModel ViewModel => this.DataContext as MapViewModel;
 
         public MapView()
         {
@@ -36,6 +36,7 @@ namespace Mapper.Controls
         private void MapGrid_MouseMove(object sender, MouseEventArgs e)
         {
             Point currentPos = e.GetPosition(MapGrid);
+            this.ViewModel.MousePosition = currentPos;
             if (this.isPanning)
             {
                 MapViewModel.Model.UpdateOriginFromMouseMovement(
@@ -44,24 +45,19 @@ namespace Mapper.Controls
                 this.lastPanPosition = currentPos;
             }
 
-            if (this.isMeasuring)
+            if (!Ruler.Instance.IsHidden)
             {
                 Ruler.Instance.ViewEndPoint = currentPos.ToVec2();
             }
 
             // Tooltip displaying world position
-            if (!this.worldPositionTip.IsOpen)
-                this.worldPositionTip.IsOpen = true;
-
-            this.worldPositionTip.HorizontalOffset = currentPos.X;
-            this.worldPositionTip.VerticalOffset = currentPos.Y + 20;
-            var worldPos = MapViewModel.Model.ToWorldSpace(currentPos.ToVec2());
-            this.worldPosition.Text = $"{Math.Round(worldPos.X)}, {Math.Round(worldPos.Y)}";
+            if (!this.ViewModel.WorldPositionPopupEnabled)
+                this.ViewModel.WorldPositionPopupEnabled = true;
         }
 
         private void MapGrid_MouseLeave(object sender, MouseEventArgs e)
         {
-            //this.worldPositionTip.IsOpen = false;
+            this.ViewModel.WorldPositionPopupEnabled = false;
         }
 
         private void MapGrid_MouseEnter(object sender, MouseEventArgs e)
@@ -78,23 +74,19 @@ namespace Mapper.Controls
         private void MapGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             var mousePos = e.GetPosition(MapGrid);
-            Keyboard.Focus(MapGrid);
-            if (isMeasuring)
+            if (!Ruler.Instance.IsHidden)
             {
-                isMeasuring = false;
-                Models.Ruler.Instance.IsHidden = true;
+                Ruler.Instance.IsHidden = true;
             }
             else if (Keyboard.IsKeyDown(Key.LeftCtrl))
             {
-                var ruler = Models.Ruler.Instance;
-                ruler.ViewStartPoint = ruler.ViewEndPoint = mousePos.ToVec2();
-                ruler.IsHidden = false;
-                isMeasuring = true;
+                Ruler.Instance.ViewStartPoint = Ruler.Instance.ViewEndPoint = mousePos.ToVec2();
+                Ruler.Instance.IsHidden = false;
             }
             else
             {
                 isPanning = true;
-                this.lastPanPosition = e.GetPosition(MapGrid);
+                this.lastPanPosition = mousePos;
             }
         }
 
@@ -121,11 +113,10 @@ namespace Mapper.Controls
 
         private void ShowMarkerTip(FrameworkElement target)
         {
-            this.markerTip.PlacementTarget = target;
-            this.markerTip.Placement = System.Windows.Controls.Primitives.PlacementMode.Center;
-            this.markerTip.IsOpen = true;
+            this.MarkerTip.PlacementTarget = target;
+            this.MarkerTip.IsOpen = true;
 
-            this.markerTip.VerticalOffset = -25;
+            this.MarkerTip.VerticalOffset = -25;
 
             var markerData = target.DataContext as MarkerViewModel;
             this.markerName.Text = markerData.Model.Name;
@@ -141,12 +132,12 @@ namespace Mapper.Controls
 
         private void Image_MouseLeave(object sender, MouseEventArgs e)
         {
-            this.markerTip.IsOpen = false;
+            this.MarkerTip.IsOpen = false;
         }
 
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            this.markerTip.IsOpen = false;
+            this.MarkerTip.IsOpen = false;
         }
 
         private void Image_MouseUp(object sender, MouseButtonEventArgs e)
--- Mapper/ViewModels/MapViewModel.cs
+++ Mapper/ViewModels/MapViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Mapper.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows;
 
 namespace Mapper.ViewModels
 {
@@ -21,9 +23,65 @@ namespace Mapper.ViewModels
         public static Map Model => Map.Instance;
         public static RulerViewModel Ruler => new();
 
+        #region World position popup properties
+        private Point mousePosition;
+        /// <summary>
+        /// Mouse position in view space.
+        /// </summary>
+        public Point MousePosition
+        {
+            get => mousePosition;
+            set
+            {
+                mousePosition = value;
+                OnPropertyChanged("MousePosition");
+                OnPropertyChanged("FormattedWorldPosition");
+                OnPropertyChanged("WorldPositionPopupHorizontalOffset");
+                OnPropertyChanged("WorldPositionPopupVerticalOffset");
+            }
+        }
+
+        private bool worldPositionPopupEnabled = true;
+        /// <summary>
+        /// Set to true if the popup showing world position at the mouse should be enabled,
+        /// and false if it should be hidden.
+        /// </summary>
+        public bool WorldPositionPopupEnabled
+        {
+            get => worldPositionPopupEnabled;
+            set
+            {
+                worldPositionPopupEnabled = value;
+                OnPropertyChanged("WorldPositionPopupEnabled");
+            }
+        }
+
+        /// <summary>
+        /// Display the world position on the form (xxx, yyy)
+        /// </summary>
+        public string FormattedWorldPosition
+        {
+            get
+            {
+                var worldPos = Map.Instance.ToWorldSpace(MousePosition.ToVec2());
+                return $"{Math.Round(worldPos.X)}, {Math.Round(worldPos.Y)}";
+            }
+        }
+
+        /// <summary>
+        /// Horizontal offset of the world position popup (relative to mouse)
+        /// </summary>
+        public double WorldPositionPopupHorizontalOffset => MousePosition.X;
+
+        /// <summary>
+        /// Vertical offset of the world position popup (relative to mouse)
+        /// </summary>
+        public double WorldPositionPopupVerticalOffset => MousePosition.Y + 20;
+        #endregion
+
 #pragma warning disable CA1822 // Mark members as static
         public ObservableCollection<MarkerViewModel> Markers =>
             new(Model.Markers.Select(marker => new MarkerViewModel(marker)));
-#pragma warning restore CA1822 // Mark members as static
+#pragma warning restore CA1822
     }
 }
